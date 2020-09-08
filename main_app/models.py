682e6e34b18ccca284f079dedcd08fe64c9ec414
User <user@Users-MBP.hitronhub.home>
2020-09-08 11:19:25 -0400

    user authentication

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,32 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class DoctorUserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete = models.CASCADE)
+
+    gender_of_user = [
+        ('Male', 'Male'),
+        ('Female', 'Female'),
+        ('Rather Not Say', 'Rather Not Say')
+    ]
+    type = 'Doctor'
+    gender = models.CharField(max_length=15, choices = gender_of_user)
+    education = models.CharField(max_length=150)
+
+    def __str__(self):
+        return f'{self.user}'
+
+class PatientUserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete = models.CASCADE)
+
+    gender_of_user = [
+        ('Male', 'Male'),
+        ('Female', 'Female'),
+        ('Rather Not Say', 'Rather Not Say')
+    ]
+    gender = models.CharField(max_length=15, choices = gender_of_user)
+    doctor = models.ForeignKey(DoctorUserProfile, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.user}'
